0708e908ab019f3b6efc158b1d6d9dde101f8475
jayanta1987 <51797031+jayanta1987@users.noreply.github.com>
2025-08-31 19:25:45 +0530

    added mongo debugs

--- src/main/java/com/jtradebot/processor/JtradebotProcessorApplication.java
+++ src/main/java/com/jtradebot/processor/JtradebotProcessorApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -14,7 +13,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @EnableCaching
 @ComponentScan(basePackages = {"com.jtradebot.processor", "com.jtradebot.tickstore"})
-@EnableMongoRepositories(basePackages = {"com.jtradebot.processor.repository", "com.jtradebot.tickstore.repository"})
 public class JtradebotProcessorApplication {
     public static void main(String[] args) {
         SpringApplication.run(JtradebotProcessorApplication.class, args);
